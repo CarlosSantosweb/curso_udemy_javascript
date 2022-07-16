230edab7f2f75e131ab982672c11fd0dc688aaa5
José Carlos <99801172+CarlosSantosweb@users.noreply.github.com>
2022-07-15 21:39:26 -0300

    atividade sobre variaveis javascript

---
+++ atividade/ex002/js/conf.js
@@ -0,0 +1,14 @@
+//string
+var texto = 'ERRO! De conexão'
+
+//number
+var numberInteiro = -7
+var numberFracionado = 10.5
+
+//boolean
+var teste = true //verdadeiro
+var test = false //falso
+
+//alert (texto) abrir um dialog
+//document.write(texto)
+//console.log(numberInteiro)
