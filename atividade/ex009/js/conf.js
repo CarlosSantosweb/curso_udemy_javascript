9389c811e2181297f85c7cbf068944fdbaeaee01
José Carlos <99801172+CarlosSantosweb@users.noreply.github.com>
2022-07-20 16:26:19 -0300

    ativida sobre operadores aritemitcos com javascript

---
+++ atividade/ex009/js/conf.js
@@ -0,0 +1,10 @@
+var num1 = 12
+var num2 = 13
+document.write(`A soma entre ` + num1 + ` e ` + num2 + ` é: ` + (num1 + num2) + `<br/>`)
+document.write(`A subtração entre ` + num1 +  ` e ` + num2 + ` é: ` + (num1 - num2) + `<br/>`)
+document.write(`A multiplicação entre ` + num1 + ` e ` + num2 + ` é: ` + (num1 * num2) + `<br/>`)
+document.write(`A divisão entre ` + num1 + ` e ` + num2 + ` é: ` + (num1 / num2) + `<br/>`)
+document.write(`A módulo entre ` + num1 + ` e ` + num2 + ` é: ` + (num1 % num2) + `<br/>`)
+document.write(`A incremento de ` + num1 + ` é: ` + (++num1) + `<br/>`)
+document.write(`A decremento de ` + num1 + ` é: ` + (--num1) + `<br/>`)
+document.write(num1)
