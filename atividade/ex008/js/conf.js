05cb4593cb3d0acdbfe86a8a09a649696ccac8c5
José Carlos <99801172+CarlosSantosweb@users.noreply.github.com>
2022-07-20 15:42:47 -0300

    atividade switch javascript

---
+++ atividade/ex008/js/conf.js
@@ -0,0 +1,14 @@
+var parametro = 2
+
+switch (parametro) {
+    case 1:
+        document.write('parametro 1')
+        break
+    
+    case 2:
+        document.write('parametro 2')
+        break
+    
+    default:
+        document.write('não encontrado')
+}
